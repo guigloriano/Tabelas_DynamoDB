a8dc713d75478a8325ff334b76737262d90670c6
Guilherme Gloriano <guilherme_gloriano@hotmail.com>
2019-10-31 07:47:33 -0400

    cont. dos teste com os graficos em R

--- RProject/graphic_test.R
+++ RProject/graphic_test.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(magrittr)
 library(readr)
 library(tidyverse)
+library(ggplot2)
+
 
 fileDia <- 20191003
 csvPath <- "D:/github/Tabelas_DynamoDB/merge/daily/Inv_Est_Merge_"
@@ -16,7 +18,14 @@ surveys_complete = as.data.frame(dataset)
 
 
 ggplot(data = surveys_complete, 
-       mapping = aes(x = surveys_complete$hora_minuto, y = surveys_complete$temperatura,
-                                                           fill=vento_dir)) + 
-                         geom_bar(position=position_dodge(), stat="identity")
+       mapping = aes(x = hora_minuto, y = pm1_massa, group = 1)) + 
+                         geom_line(color="grey", size=1) + 
+                         geom_point(shape=21, color="black", fill="#69b3a2", size=2) +
+                         theme(axis.text.x = element_text(angle = 90)) +
+                         labs(x = "Hora", y = "Massa PM1.0") +
+                         ggtitle("Distribuição de Massa PM1.0", center)
 
+
+
+#ggplot(data = surveys_complete, mapping = aes(x = hora_minuto, y = pm1_massa, fill=vento_dir)) + 
+#  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90))
