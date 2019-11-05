6168a66c751d627dce6b3391c447efdd57f2b7f6
Guilherme Gloriano <guilherme_gloriano@hotmail.com>
2019-11-04 23:32:06 -0400

    modificacoes para reuniao do dia 05/10/19

--- RProject/graphic_test.R
+++ RProject/graphic_test.R
@@ -9,10 +9,10 @@ library(reshape2)
 csvPath <- setwd("D:\\github\\Tabelas_DynamoDB\\merge\\daily\\")
 names <- list.files(pattern = "*.csv")
 
-#for(i in 1:length(names)){ 
-  assign(names[2],read.csv(names[2],skip=1, header=TRUE))
+for(i in 1:length(names)){ 
+  assign(names[i],read.csv(names[i],skip=1, header=TRUE))
   
-  dataset <- readr::read_csv(names[2], col_types = cols(hora_minuto = col_character()))
+  dataset <- readr::read_csv(names[i], col_types = cols(hora_minuto = col_character()))
   
   dados_graf = as.data.frame(dataset)
   
@@ -34,17 +34,18 @@ names <- list.files(pattern = "*.csv")
 
   p <- ggplot(data = dados_graf, mapping = aes(x = hora_minuto)) + 
     geom_line(aes(y=temperatura, color="temperatura", group=1)) +
+    
     geom_line(aes(y=P_AC/100, color="P_AC", group=2)) + 
+    geom_line(aes(y=P_DC/100, color="P_DC", group=3)) + 
     
+    geom_line(aes(y=pm1_concentracao, color="pm1_concentracao", group=4)) + 
+    geom_line(aes(y=pm2_concentracao, color="pm2_concentracao", group=5)) + 
+  
+    geom_line(aes(y=vento_dir/2, color="vento_dir", group=6)) + 
+    geom_line(aes(y=preciptacao, color="preciptacao", group=7)) + 
     
-    geom_line(aes(y=P_DC/100, color="P_DC", group=2)) + 
-  #  geom_line(aes(y=pm1_concentracao, color="pm1_concentracao", group=3)) + 
-  #  geom_line(aes(y=vento_dir/2, color="vento_dir", group=4)) + 
-  #  geom_line(aes(y=preciptacao, color="preciptacao", group=4)) + 
-    
-    geom_line(aes(y=irradiacao/10, color="irr_est", group=2)) + 
-    
-    geom_line(aes(y=IRR/10, color="irr_inv", group=2, )) +
+    geom_line(aes(y=irradiacao/10, color="irr_est", group=8)) + 
+    geom_line(aes(y=IRR/10, color="irr_inv", group=9, )) +
     
     scale_y_continuous(sec.axis = sec_axis(~.*10, name = "POT. AC")) +
     theme(axis.text.x = element_text(angle = 90)) +
@@ -56,10 +57,10 @@ names <- list.files(pattern = "*.csv")
   dia <- dados_graf$dia_mes_ano[1]
   
   pathDest <- "D:/github/Tabelas_DynamoDB/merge/graph/teste/"
-  fileDest <- paste(pathDest,  "TESTE2__", dia, ".png", sep = "")
+  fileDest <- paste(pathDest,  "Todas_Variaveis_", dia, ".png", sep = "")
   
   png(filename = fileDest, width = 1024, height = 800, units = 'px')
   plot(p)
   dev.off()
   
-#}
+}
